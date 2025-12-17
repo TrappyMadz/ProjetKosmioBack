ab3a6af05cb21ab176594d230a8eb7d9734660af
Charrier Simon <simon.charrier42@outlook.fr>
2025-12-17 14:53:56 +0100

    Resolving conflict and merging

--- src/main/run.py
+++ src/main/run.py
@@ -2,11 +2,8 @@ from controller import rag_controller
 import os
 import json
 from dotenv import load_dotenv
-<<<<<<< HEAD
 import uvicorn
-=======
 from service.bdd_service.bdd_service import PostgresService
->>>>>>> CrudFunctionsPastLlm
 
 app = rag_controller.rag_app
 
